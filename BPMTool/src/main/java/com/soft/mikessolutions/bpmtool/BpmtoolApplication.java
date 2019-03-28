bf84fc9fb8a3defe73e6664527cfdad979694b45
Michal Badura <badura.mike@gmail.com>
2019-03-28 20:49:28 +0100

    Initial commit

---
+++ BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/BpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.soft.mikessolutions.bpmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmtoolApplication.class, args);
+    }
+
+}
