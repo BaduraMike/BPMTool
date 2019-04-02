404086ac737c4552be79d6193305288347a08d63
Michal Badura <badura.mike@gmail.com>
2019-04-02 20:59:51 +0200

    Add @Valid, BindingResult to createNewProject

--- BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
+++ BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
@@ -4,7 +4,10 @@ import com.soft.mikessolutions.bpmtool.entities.Project;
 import com.soft.mikessolutions.bpmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -16,7 +19,12 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+        }
+
         projectService.save(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
