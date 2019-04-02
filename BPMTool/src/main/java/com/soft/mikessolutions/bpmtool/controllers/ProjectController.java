3cc69afba167f20fa646a75bf62d0588c5a1ab20
Michal Badura <badura.mike@gmail.com>
2019-04-02 21:33:05 +0200

    Add errorMap to createNewProject Response

--- BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
+++ BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
@@ -5,9 +5,13 @@ import com.soft.mikessolutions.bpmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,11 +24,13 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid object", HttpStatus.BAD_REQUEST);
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
-
         projectService.save(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
