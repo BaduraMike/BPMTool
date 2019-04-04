e7a898934d0a5f86559367a7734dc22934646f21
Michal Badura <badura.mike@gmail.com>
2019-04-05 00:17:03 +0200

    Add deleteByProjectIdentifier.

--- BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
+++ BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
@@ -9,10 +9,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/project")
+@RequestMapping("/api/projects")
 public class ProjectController {
     private final ProjectService projectService;
     private final ValidationErrorService validationErrorService;
@@ -32,8 +33,19 @@ public class ProjectController {
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 
+    @GetMapping("")
+    public List<Project> findAllProjects() {
+        return projectService.findAll();
+    }
+
     @GetMapping("/{projectIdentifier}")
     public ResponseEntity<?> getProjectByIdentifier(@PathVariable String projectIdentifier) {
         return new ResponseEntity<Project>(projectService.findByProjectIdentifier(projectIdentifier), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{projectIdentifier}")
+    public ResponseEntity<?> deleteProjectByIdentifier(@PathVariable String projectIdentifier) {
+        projectService.deleteByProjectIdentifier(projectIdentifier);
+        return new ResponseEntity<String>("Project with {IDENTIFIER} '" + projectIdentifier.toUpperCase() + "' is deleted.", HttpStatus.OK);
     }
 }
