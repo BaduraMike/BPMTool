75be74ba957d2570153f1e4af1d6ef83661906e6
Michal Badura <badura.mike@gmail.com>
2019-04-07 22:59:17 +0200

    Add bpmtool-react-client - initial commit.

---
+++ bpmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react';
+
+class Dashboard extends Component {
+    propTypes = {};
+
+    defaultProps = {};
+
+    render() {
+        return (
+            <h4>Welcome to the Dashboard</h4>
+        );
+    }
+}
+
+export default Dashboard;
