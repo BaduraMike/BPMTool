02ba48edb4ee98d88e5c93e274c55382de50b309
Michal Badura <badura.mike@gmail.com>
2019-04-14 22:56:15 +0200

    Add creating Project from React level

--- BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
+++ BPMTool/src/main/java/com/soft/mikessolutions/bpmtool/controllers/ProjectController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
+@CrossOrigin
 public class ProjectController {
     private final ProjectService projectService;
     private final ValidationErrorService validationErrorService;
